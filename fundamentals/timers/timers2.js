d5010869e3c057cd5488465e1697a9bc285a612d
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-12-02 04:33:49 +0530

    Add fundamentals/timers/timers2.js

---
+++ fundamentals/timers/timers2.js
@@ -0,0 +1,23 @@
+const baz = () => console.log('baz');
+const foo = () => console.log('foo');
+const zoo = () => console.log('zoo');
+const start = () => {
+	console.log('start');
+	setImmediate(baz);
+	new Promise((resolve, reject) => {
+		resolve('bar');
+	}).then(resolve => {
+		console.log(resolve);
+		process.nextTick(zoo);
+	});
+	process.nextTick(foo);
+};
+
+start();
+
+// This will log:
+// start
+// foo
+// bar
+// zoo
+// baz
