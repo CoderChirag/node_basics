3b4d6639920eacb694dbf8ed830c3c61dbac696e
coderchirag <jain.chirag0174@gmail.com>
2023-08-23 23:58:28 +0530

    fresh start

--- 7. worker threads/worker.js
+++
@@ -1,5 +0,0 @@
-const { workerData, parentPort } = require('worker_threads');
-
-console.log('Worker Thread implementation by ' + workerData);
-
-parentPort.postMessage({ fileName: workerData, status: 'Done' });
